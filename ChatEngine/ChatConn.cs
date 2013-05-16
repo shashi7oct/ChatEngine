2d7390f4474a7adc0a04fedf75d57d6556d608b3
Shashi Prakash <shashi7oct@yahoo.com>
2013-05-16 21:02:55 +0530

    simple chat using SignalR

---
+++ ChatEngine/ChatConn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatEngine
+{
+    public class ChatConn : Hub
+    {
+        public void sendmsg(string msg)
+        {
+            Clients.All.sendmessage(Context.ConnectionId, msg);
+        }
+    }
+}
---
+++ ChatEngine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatEngine.Global" Language="C#" %>
